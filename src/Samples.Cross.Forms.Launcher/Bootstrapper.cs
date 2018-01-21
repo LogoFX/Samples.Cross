f4f3ca3c24f743b5719ce5df7e2b1130cc3469fe
Gennady Verdel <godrose@gmail.com>
2018-01-21 18:18:31 +0200

    Added Forms Launcher - Root view model to be added

---
+++ src/Samples.Cross.Forms.Launcher/Bootstrapper.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Solid.Bootstrapping;
+using Solid.Extensibility;
+using Solid.Practices.Composition;
+using Solid.Practices.Composition.Container;
+using Solid.Practices.Composition.Contracts;
+using Solid.Practices.IoC;
+using Solid.Practices.Middleware;
+using Solid.Practices.Modularity;
+
+namespace Samples.Cross.Forms.Launcher
+{
+    public class Bootstrapper : IInitializable,
+        IExtensible<Bootstrapper>,
+        ICompositionModulesProvider,
+        IHaveRegistrator,
+        IAssemblySourceProvider
+    {
+        private readonly
+            List<IMiddleware<Bootstrapper>>
+            _middlewares =
+                new List<IMiddleware<Bootstrapper>>();
+
+        public Bootstrapper(IDependencyRegistrator dependencyRegistrator)
+        {
+            Registrator = dependencyRegistrator;
+            PlatformProvider.Current = new NetStandardPlatformProvider();
+        }
+
+        /// <summary>
+        /// Gets the list of modules that were discovered during bootstrapper configuration.
+        /// </summary>
+        /// <value>
+        /// The list of modules.
+        /// </value>
+        public IEnumerable<ICompositionModule> Modules { get; private set; } = new ICompositionModule[] { };
+
+        public IDependencyRegistrator Registrator { get; }
+
+        //TODO: Load assemblies by prefix
+        public IEnumerable<Assembly> Assemblies => new Assembly[]
+            {
+                //Assembly.GetAssembly(typeof(ShellViewModel)),
+                //Assembly.GetAssembly(typeof(HelloXamarinForms.Model.Module)),
+                //Assembly.GetAssembly(typeof(HelloXamarinForms.Data.Fake.Providers.Module)),
+                //Assembly.GetAssembly(typeof(HelloXamarinForms.Data.Fake.ProviderBuilders.Module))
+            };
+
+        private void InitializeCompositionModules()
+        {
+            //TODO: File-Based assembly loading does not work in mobile platforms yet     
+            ICompositionContainer<ICompositionModule> innerContainer = new SimpleCompositionContainer<ICompositionModule>(
+                Assemblies,
+                new ActivatorCreationStrategy());
+            innerContainer.Compose();
+            Modules = innerContainer.Modules.ToArray();
+        }
+
+        /// <summary>
+        /// Extends the functionality by using the specified middleware.
+        /// </summary>
+        /// <param name="middleware">The middleware.</param>
+        /// <returns></returns>
+        public Bootstrapper Use(
+            IMiddleware<Bootstrapper> middleware)
+        {
+            _middlewares.Add(middleware);
+            return this;
+        }
+
+        public void Initialize()
+        {
+            InitializeCompositionModules();
+            MiddlewareApplier.ApplyMiddlewares(this, _middlewares);
+        }
+    }
+}
---
+++ src/Samples.Cross.Forms.Launcher/FormsApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Caliburn.Micro.Xamarin.Forms;
+using LogoFX.Bootstrapping;
+using LogoFX.Client.Bootstrapping;
+using Solid.Practices.IoC;
+using Xamarin.Forms;
+
+namespace Samples.Cross.Forms.Launcher
+{
+    public class FormsApp : FormsApplication
+    {
+        private readonly IDependencyRegistrator _dependencyRegistrator;
+
+        public FormsApp(IDependencyRegistrator dependencyRegistrator)
+        {
+            Initialize();
+
+            _dependencyRegistrator = dependencyRegistrator;
+            var bootstrapper =
+                new Bootstrapper(_dependencyRegistrator)
+                    .Use(new RegisterCompositionModulesMiddleware<Bootstrapper>())
+                    .Use(new RegisterViewModelsMiddleware<Bootstrapper>(new Type[] { }));
+            bootstrapper.Initialize();
+            //DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override void PrepareViewFirst(NavigationPage navigationPage)
+        {
+            _dependencyRegistrator.RegisterInstance<INavigationService>(new NavigationPageAdapter(navigationPage));
+        }
+    }
+}
