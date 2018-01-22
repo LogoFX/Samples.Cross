78af49c6ea3f7dfa1bd9a9d72e0b4a0877a2620a
Gennady Verdel <godrose@gmail.com>
2018-01-22 10:18:35 +0200

    Missing changes

--- src/Samples.Cross.Forms.Presentation.Shell/ViewModels/ShellViewModel.cs
+++ src/Samples.Cross.Forms.Presentation.Shell/ViewModels/ShellViewModel.cs
@@ -1,8 +1,24 @@
 ﻿using Caliburn.Micro;
+using Samples.Cross.Model.Contracts;
 
 namespace Samples.Cross.Forms.Presentation.Shell.ViewModels
 {
-    public class ShellViewModel : PropertyChangedBase
+    public class ShellViewModel : Conductor<object>.Collection.OneActive
     {
+        private readonly ILoginService _loginService;
+
+        public ShellViewModel(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        protected override void OnInitialize()
+        {
+            base.OnInitialize();
+            //TODO: Use View Model factory
+            var loginViewModel = new LoginViewModel(_loginService);
+            Items.Add(loginViewModel);
+            ActivateItem(loginViewModel);
+        }
     }
 }
