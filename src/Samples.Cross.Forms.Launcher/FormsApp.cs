2783c4000beb02302e62facedf2e6962550bd5f0
Gennady Verdel <godrose@gmail.com>
2018-03-06 10:29:02 +0200

    Using latest version of LogoFX.Client.Bootstrapping.Xamarin.Forms

--- src/Samples.Cross.Forms.Launcher/FormsApp.cs
+++ src/Samples.Cross.Forms.Launcher/FormsApp.cs
@@ -1,15 +1,13 @@
 ﻿using LogoFX.Client.Bootstrapping.Xamarin.Forms;
 using Samples.Cross.Forms.Presentation.Shell.ViewModels;
-using Solid.Practices.IoC;
 
 namespace Samples.Cross.Forms.Launcher
 {
     public class FormsApp : LogoFXApplication<ShellViewModel>
     {
-        public FormsApp(BootstrapperBase bootstrapper, IDependencyRegistrator dependencyRegistrator) :
+        public FormsApp(BootstrapperBase bootstrapper) :
         base(
-            bootstrapper.UseMiddlewares(), 
-            dependencyRegistrator)
+            bootstrapper.UseMiddlewares())
         {            
         }
     }
